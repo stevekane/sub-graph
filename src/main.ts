a6dd68b4db257ee88235aa81d0e7be3671421563
Steve Kane <kanesteven@gmail.com>
2016-10-06 16:44:59 -0500

    major functional overhaul

--- src/main.ts
+++ src/main.ts
@@ -25,71 +25,43 @@ class Module<T> implements Functor<T>, Foldable<T> {
   }
 }
 
-// // Traverse a sub-graph looking for first satisfying connection
-// function findConnection (uuid: UUID, m: Module<IScene>): UUID | null {
-//   const connection = m.connections.get(uuid)
-// 
-//   if ( connection != null ) return connection
-// 
-//   for ( const cm of m.modules ) {
-//     const c = findConnection(uuid, cm)
-//     
-//     if ( c != null ) return c
-//   }
-//   return null
-// }
-// 
-// // Traversal is a pruned sub-graph w/ only the modules needed to get to the predicate-satisfying scene
-// function findTraversal (f: (s: IScene) => boolean, m: Module<IScene>): Module<IScene> | null {
-//   for ( const s of m.scenes ) {
-//     if ( f(s) ) return new Module(m.scenes, [], m.connections) 
-//   }
-//   for ( const cm of m.modules ) {
-//     const t = findTraversal(f, cm)
-// 
-//     if ( t ) return new Module(m.scenes, [ t ], m.connections)
-//   }
-//   return null
-// }
-// 
-// // Traverse the module looking for next scene given current scene's UUID
-// function findNext (uuid: UUID, m: Module<IScene>): IScene | null {
-//   const traversal = findTraversal(s => s.uuid == uuid, m)
-// 
-//   if ( traversal == null ) return null
-// 
-//   const nextUUID = findConnection(uuid, traversal)
-// 
-//   return nextUUID ? findWhere(s => s.uuid == nextUUID, m) : null
-// }
+function findTraversal<T> (f: (s: T) => boolean, m: Module<T>): Module<T>[] {
+  if ( m.leaves.filter(f).length ) return [ m ]
+  else { 
+    const path = concatMap(cm => findTraversal(f, cm), m.modules)
+
+    if ( path.length == 0 ) return []
+    else                    return [ m ].concat(path)
+  }
+}
+
+function concatMap<A, B> (f: (a: A) => B[], list: A[]): B[] {
+  return list.map(f).reduce((out, each) => out.concat(...each), [])
+}
 
 function leavesWhere<T> (f: (t: T) => boolean, m: Module<T>): T[] {
-  return m.reduce((l, m) => m.leaves.filter(f), [] as T[])
+  return m.reduce((l, em) => l.concat(em.leaves.filter(f)), [] as T[])
 }
 
 function connectionsFor (uuid: UUID, m: Module<IScene>): UUID[] {
-  return m.reduce((l, m) => l.concat(m.connections.has(uuid) ? m.connections.get(uuid) as UUID : []), [] as UUID[])
+  return m.reduce((l, em) => l.concat(em.connections.has(uuid) ? em.connections.get(uuid) as UUID : []), [] as UUID[])
+}
+
+function findNext (uuid: UUID, m: Module<IScene>): IScene | null {
+  const traversal = findTraversal(l => l.uuid == uuid, m)
+  const nextUUID = connectionsFor(uuid, m)[0]
+
+  return nextUUID ? leavesWhere(s => s.uuid == nextUUID, m)[0] : null
 }
 
 const m1 = new Module([ { uuid: 2 }, { uuid: 3 } ], [], new Map([ [ 2, 3 ] ]))
 const m2 = new Module([ { uuid: 4 }, { uuid: 5 } ], [], new Map([ [ 4, 5 ] ]))
 const e = new Module([ { uuid: 0 }, { uuid: 1 }, { uuid: 6 } ], [ m1, m2 ], new Map([ [ 0, 1 ], [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]))
-const totalLeaves = e.reduce((l, m) => l + m.leaves.length, 0)
-const uuids = e.reduce((l, m) => l.concat(m.leaves.map(l => l.uuid)), [] as UUID[])
-const consFor1 = connectionsFor(1, e)
-const withUUID5 = leavesWhere(l => l.uuid == 5, e)
+const fn = findNext(4, e)
 
-console.log(totalLeaves)
-console.log(uuids)
-console.log(consFor1)
-console.log(withUUID5)
-// 
-// var n: IScene | null = e.scenes[0]
-// 
-// // console.log(findTraversal(x => x.uuid == 3, e))
-// // console.log(findTraversal(x => x.uuid == 5, e))
-// 
-// console.log(e.scenes[0])
-// while ( n = findNext(n.uuid, e) ) {
-//   console.log(n)
-// }
+var uuid = 0
+var n: null | IScene = e.leaves[0]
+
+while (n = findNext(uuid++, e)) {
+  console.log(n)
+}
